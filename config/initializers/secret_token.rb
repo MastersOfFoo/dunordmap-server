80c761a0172fbd1929e81c43c45c0854ef4c7f17
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-11-15 10:51:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DunordmapServer::Application.config.secret_token = 'd5cbf79c427019e8b99da244eff0339c11a0878fb19729d3141381d1536d795dd528ab47e9e4b02ecd7dc157c62dd814a2e0667903e0a0d3d437596414722ec4'
